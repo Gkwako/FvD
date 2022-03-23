6cae7db0c0d192c8d1d92c117910f9e1f7387992
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-03-23 15:38:31 +0100

    change 23-03

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -57,62 +57,62 @@ console.log("howdy");
 
 // mp3 songs
 
-let all_song = [
-    {
-        name: "2 step",
-        path: "muziek/Ed Sheeran - 2step .mp3",
-        img: "images/2Step.png",
-        singer: "Ed Sheeran"
-    },
-    {
-        name: "if i killed...you",
-        path: "muziek/Alec Benjamin - If I Killed Someone For You .mp3",
-        img: "images/alec.jpg",
-        singer: "Alec Benjamin"
-    },
-    {
-        name: "Ghost Town",
-        path: "muziek/Benson Boone - Ghost Town.mp3",
-        img: "images/benson.png",
-        singer: "Benson Boone"
-    },
-    {
-        name: "Half a man",
-        path: "muziek/Dean Lewis - Half A Man.mp3",
-        img: "images/deanLewis.jpg",
-        singer: "Ed Sheeran"
-    },
-    {
-        name: "Better Days",
-        path: "muziek/Dermot Kennedy - Better Days.mp3",
-        img: "images/dermotKennedy.jpg",
-        singer: "Dermot Kennedy"
-    },
-    {
-        name: "Officer Don't Let Go",
-        path: "muziek/Faffi - Officer Don't Let Go.mp3",
-        img: "images/jefferson.jpg",
-        singer: "Faffi"
-    }
-];
+// let all_song = [
+//     {
+//         name: "2 step",
+//         path: "muziek/Ed Sheeran - 2step .mp3",
+//         img: "images/2Step.png",
+//         singer: "Ed Sheeran"
+//     },
+//     {
+//         name: "if i killed...you",
+//         path: "muziek/Alec Benjamin - If I Killed Someone For You .mp3",
+//         img: "images/alec.jpg",
+//         singer: "Alec Benjamin"
+//     },
+//     {
+//         name: "Ghost Town",
+//         path: "muziek/Benson Boone - Ghost Town.mp3",
+//         img: "images/benson.png",
+//         singer: "Benson Boone"
+//     },
+//     {
+//         name: "Half a man",
+//         path: "muziek/Dean Lewis - Half A Man.mp3",
+//         img: "images/deanLewis.jpg",
+//         singer: "Ed Sheeran"
+//     },
+//     {
+//         name: "Better Days",
+//         path: "muziek/Dermot Kennedy - Better Days.mp3",
+//         img: "images/dermotKennedy.jpg",
+//         singer: "Dermot Kennedy"
+//     },
+//     {
+//         name: "Officer Don't Let Go",
+//         path: "muziek/Faffi - Officer Don't Let Go.mp3",
+//         img: "images/jefferson.jpg",
+//         singer: "Faffi"
+//     }
+// ];
 
-let tracks = document.querySelector('.tracks');
+// let tracks = document.querySelector('.tracks');
 
-for (let i = 0; i < all_song.length; i++){
+// for (let i = 0; i < all_song.length; i++){
 
-    let html = ` <div class="song">
-        <div class="img">
-        <img src="${all_song[i].img}"/
-        </div> 
-        <div class="more">
-        <audio src="${all_song[i].path}" id="music"></audio>
-        <div class="song_info">
-            <p id="title">${all_song[i].name}</p>
-            <p>${all_song[i].singer}</p>
-        </div>
-        <button id="play_btn"><i class=fa fa-angle-right" aria-hidden="true"></i></button>
-        </div>
-        </div>`;
+//     let Html = ` <div class="song">
+// 	<div class="img">
+// 	<img src="${all_song[i].img}">
+// 	</div>
+// 	<div class="more">
+// 	<audio src="${all_song[i].path}" id="music"></audio>
+// 	<div class="song_info">
+// 		<p id="title">${all_song[i].name}</p>
+// 		<p>${all_song[i].singer}</p>
+// 	</div>
+// 	<button id="play_btn"><i class=fa fa-angle-right aria-hidden="true"></i></button>
+// 	</div>
+// 	</div>`;
 
-        tracks.insertAdjacentHTML("beforeend", html);
-};
+//         tracks.insertAdjacentHTML("beforeend", Html);
+// };
