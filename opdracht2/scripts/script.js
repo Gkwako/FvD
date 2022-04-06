d68e2e0edfe32ca25ad38db132ec5267be5b1683
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-04-06 11:43:07 +0200

    new change 6/4

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,146 +1,80 @@
 // JavaScript Document
-console.log("howdy");
+console.log("Music Time");
+
+var zoekbalk = document.querySelector(".search");
+var zoeken = document.querySelector(".searchbar");
+var kruisje = document.querySelector(".clear");
+
+zoekbalk.onclick = function(){
+  zoeken.classList.toggle('active')
+  kruisje.classList.toggle('active')
+}
+
+kruisje.onclick = function(){
+  kruisje.classList.toggle('active')
+  zoeken.classList.toggle('active')
+}
+
+
+// -------------------------------------------
 
 
 
-var options = {
-    // de tijd van swappen in ms
-    animation: 1000
-  }
+
+
+
+
+
+// var options = {
+//   animation: 1000
+// }
+
+// var playList = document.getElementById('playList');
+// var sortable = Sortable.create('playList', options);
+
+
+var fill = document.querySelector('.fill');
+var empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('start drag', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop throug empties and call drag
+for(var empty of empties){
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragLeave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+
+// Drag functions
+function dragStart() { 
+  console.log("start drag");
+      setTimeout(() => (this.className = 'invisible'), 0 );
+}
+
+function dragEnd() { 
+  console.log("end drag");
+    this.className = 'fill'; 
+}
+    function dragOver(e) { 
+      console.log("drag over");
+        e.preventDefault(); // zorgt ervoor dat img niet terug gaat
+      }
+    function dragEnter(e) { 
+      console.log("drag enter");
+      e.preventDefault(); // zorgt ervoor dat img niet terug gaat
+      this.append(fill); // fill kan voortbewegen tussen andere empty's
+ 
+    }
+    function dragLeave() { 
+      console.log("drag leave");
+        // this.className = 'empty';
+        this.append(fill); // fill kan voortbewegen tussen andere empty's
   
-  /* het daadwerkelijk initialiseren van het draggen-en-droppen */
-  // list - de ID van de ul
-  // options - hierboven gedefinieerd
-  var deLijst = document.getElementById('list');
-  var sortable = Sortable.create(deLijst, options);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const URL ="https://openwhyd.org/adrien?format=json";
-
-//haal de lijst 'ul' op uit de html
-// const list = document.querySelector('ul');
-
-
-
-// lijst
-
-// function getMusic(){
-
-//     getData(URL).then( 
-//         data => {
-//             console.log(data.allMusic);
-
-//             var allMusic = data.music;
-
-//             allMusic.forEach( 
-//                 aNumber => {
-//                     console.log(aNumber);
-
-//                     var allMusicHTML =
-//                     `
-//                     <li>
-//                     <h2>${aNumber.song},</h2>
-//                     <img src="${aNumber.song}" alt="${allMusic.song}">
-//                     <li/>
-                    
-//                     `;
-
-//                     list.insertAdjacentHTML("beforeEnd", allMusicHTML);
-//         }
-//     );
-// }
-
-// );
-
-// }
-
-// async function getData(URL) {
-// 	return (
-// 		fetch(URL, { mode: 'no-cors' } )
-// 		.then ( response => response.json() )
-// 		.then ( jsonData => jsonData )
-// 	);
-// }
-
-
-// getMusic();
-
-// list.addEventListener("click", getMusic);
-
-
-
-// mp3 songs
-
-// let all_song = [
-//     {
-//         name: "2 step",
-//         path: "muziek/Ed Sheeran - 2step .mp3",
-//         img: "images/2Step.png",
-//         singer: "Ed Sheeran"
-//     },
-//     {
-//         name: "if i killed...you",
-//         path: "muziek/Alec Benjamin - If I Killed Someone For You .mp3",
-//         img: "images/alec.jpg",
-//         singer: "Alec Benjamin"
-//     },
-//     {
-//         name: "Ghost Town",
-//         path: "muziek/Benson Boone - Ghost Town.mp3",
-//         img: "images/benson.png",
-//         singer: "Benson Boone"
-//     },
-//     {
-//         name: "Half a man",
-//         path: "muziek/Dean Lewis - Half A Man.mp3",
-//         img: "images/deanLewis.jpg",
-//         singer: "Ed Sheeran"
-//     },
-//     {
-//         name: "Better Days",
-//         path: "muziek/Dermot Kennedy - Better Days.mp3",
-//         img: "images/dermotKennedy.jpg",
-//         singer: "Dermot Kennedy"
-//     },
-//     {
-//         name: "Officer Don't Let Go",
-//         path: "muziek/Faffi - Officer Don't Let Go.mp3",
-//         img: "images/jefferson.jpg",
-//         singer: "Faffi"
-//     }
-// ];
-
-// let tracks = document.querySelector('.tracks');
-
-// for (let i = 0; i < all_song.length; i++){
-
-//     let Html = ` <div class="song">
-// 	<div class="img">
-// 	<img src="${all_song[i].img}">
-// 	</div>
-// 	<div class="more">
-// 	<audio src="${all_song[i].path}" id="music"></audio>
-// 	<div class="song_info">
-// 		<p id="title">${all_song[i].name}</p>
-// 		<p>${all_song[i].singer}</p>
-// 	</div>
-// 	<button id="play_btn"><i class=fa fa-angle-right aria-hidden="true"></i></button>
-// 	</div>
-// 	</div>`;
-
-//         tracks.insertAdjacentHTML("beforeend", Html);
-// };
+    }
+    function dragDrop() { 
+      console.log("drag drop");
+        this.className = 'empty';   
+   }
