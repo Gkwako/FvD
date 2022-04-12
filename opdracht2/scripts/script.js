3446482c6a2b50e41da3a57378c425297ca1dc31
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-04-12 12:42:37 +0200

    change12/4

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,11 +1,5 @@
 // JavaScript Document
 
-// --------------------------- dark mode
-// function darkmode() {
-//     var body = document.body;
-// 	body.classList.toggle("darkmode");
-// }
-
 // --------------------------- grab songs
 console.log("empty Time");
 
@@ -66,18 +60,167 @@ kruisje.onclick = function(){
 // ---------------------------Search song
 console.log("alfabetische volgorde songs");
 
-var options = {
-  valueNames: [ 'name', 'song' ]
+// var options = {
+//   valueNames: [ 'name', 'song' ]
+// };
+
+// var zoekLijst = new List('playList', options);
+
+
+// zoekLijst.sort('name', { order: "asc" });
+
+
+
+
+// --------------------------- API Songs
+console.log("music Time");
+
+// // api 
+// // https://www.theaudiodb.com
+// // https://www.theaudiodb.com/api_guide.php
+const urlBase = "https://theaudiodb.com/api/v1/json/2/mvid.php?i=";
+
+//lists
+const listWalker = document.querySelector('ol.t-Walker');
+const listDragons = document.querySelector('ol.i-Dragons');
+const listSmith = document.querySelector('ol.s-smith');
+
+// artists
+const TomWalker = {
+	"id": "152173",
+	"name": "Tom Walker",
+	"list": listWalker
 };
 
-var zoekLijst = new List('playList', options);
+const ImagineDragons = {
+	"id": "114415",
+	"name": "Imagine Dragons",
+	"list": listDragons
+};
+
+const SamSmith = {
+	"id": "126975",
+	"name": "Sam Smith",
+	"list": listSmith
+};
+
+// per artiest een object met daarin:
+// - de id van de artiest
+// - de naam van de artiest
+// - de lijst waar de video's van de artiest in moeten komen
 
 
-zoekLijst.sort('name', { order: "asc" });
+
+
+
+/****************/
+/* VUL DE LIJST */
+/****************/
+function getVideos(artist) {
+	// de api url
+	// de base url + de id van de artiest (uit het artist object)
+	var URL = urlBase + artist.id;
+	
+	// vraag de videos van de artist op
+	getData(URL).then(
+		data => {
+
+			// sla de lijst met video-data op
+			let allVideos = data.mvids;
+      
+			// dan elke video uit de lijst een voor een toevoegen
+			allVideos.forEach( video => {
+				
+				// de youtube-id video zijn de laatste 11 karakters van de strMusicVid info
+				const videoId = video.strMusicVid.substr(video.strMusicVid.length - 11);
+				
+				// de HTML samenstellen voor een embedded youtube video
+				// met daarin de videoId die net bepaald is
+				const iframeHTML = '<iframe width="187" height="105" src="//www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+				
+				// dit stukje is net als gisteren
+				// HTML schrijven voor de li
+				// met daarin dusnamische stukjes:
+				// - naam van nummer
+				// - naam van artiest
+				// - de HTML voor de embedded youtube video
+				let videoHtml = `
+          <li>
+            <h3>${video.strTrack}</h3>
+						<p>${artist.name}</p>
+						${iframeHTML}
+					</li>
+				`;
+				artist.list.insertAdjacentHTML("beforeEnd", videoHtml);
+			});
+		}
+	);
+}
 
 
 
 
-// --------------------------- Youtube Songs
-console.log("music Time");
+
+/****************/
+/* FETCH DATA   */
+/* RETURNS JSON */
+/****************/
+async function getData(URL) {
+	return (
+		fetch(URL)
+		.then ( response => response.json() )
+		.then ( jsonData => jsonData )
+	);
+}
+ 
+
+
+
+
+
+/*********/
+/* START */
+/*********/
+
+// vul de nirvana list
+// het object van de artiest wordt meegegeven
+getVideos(TomWalker);
+
+getVideos(ImagineDragons);
+
+getVideos(SamSmith);
+
+
+
+// var options = {
+//   animation: 1000
+// }
+
+// var playList = document.getElementById('playList');
+// var sortable = Sortable.create('playList', options);
+
+
+// const playButton = document.querySelector(".button");
+
+// var audio1 = document.querySelector("#song1");
+
+
+
+
+// function buttonStart(){
+// 	if (audio1.paused){
+// 		console.log("button play");
+// 		audio1.play();
+// 		playButton.classList.add("paused");
+// 		return false;
+
+// 	} else { (audio1.play)
+// 		console.log("button stop");
+// 		audio1.pause();
+// 		playButton.classList.remove("paused");
+// 		return false;
+// 	}
+// }
+// playButton.addEventListener('click', buttonStart)
+
 
